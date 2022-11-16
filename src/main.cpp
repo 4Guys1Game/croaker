4199225890931427d93bccca73594b3514310709
ItsGers8 <gerson@makweb.nl>
2022-11-16 12:56:41 +0100

    added cmakelists, avr compile settings and a basic main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * A frogger implementation for Arduino.
+ */
+// keep intellisense satisfied
+#ifndef __AVR_ATmega328P__
+    #define __AVR_ATmega328P__
+#endif
+
+// Header file needed to enable interrupts.
+#include <avr/interrupt.h>
+
+int main(void)
+{
+    while (1) {}
+    return 0;
+}
