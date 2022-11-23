214855e84a8ef5f2302523afadfa3f4916fea236
jaschutte <jaschutte49@gmail.com>
2022-11-23 09:11:06 +0100

    flat image drawing

--- lib/gfx.cpp
+++ lib/gfx.cpp
@@ -7,10 +7,94 @@
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-void init_gfx() {
-	tft.begin(); // Initialize the tft
+// Draw a bitmap on the screen
+void draw_bitmap(uint16_t *bitmap, Vector2 *position, Vector2 *size)
+{
+	tft.drawRGBBitmap(position->x, position->y, bitmap, size->x, size->y);
 }
 
-Adafruit_ILI9341 get_tft() {
-	return tft;
+// Draw a specified rectangle of a bitmap on the screen
+void draw_bitmap_rect(uint16_t *bitmap, Vector2 position, Vector2 *size, Vector2 *rect_pos, Vector2 *rect_size)
+{
+	for (uint16_t h = 0; h < rect_size->y; h++)
+	{
+		for (uint16_t w = 0; w < rect_size->x; w++)
+		{
+			Vector2 img_pos = {rect_pos->x + w, rect_pos->y + h};
+			tft.drawPixel(position.x + w, position.y + h, VECTOR2_TO_COLOR(bitmap, img_pos, size));
+		}
+	}
+}
+
+// Same as draw_bitmap, but handles bitmaps stored in PROGMEM
+void draw_bitmap_P(uint16_t *bitmap, Vector2 *position, Vector2 *size)
+{
+	for (uint16_t h = 0; h < size->y; h++)
+	{
+		for (uint16_t w = 0; w < size->x; w++)
+		{
+			Vector2 img_pos = {w, h};
+			uint16_t color = pgm_read_dword(VECTOR2_TO_PTR(bitmap, img_pos, size));
+			tft.drawPixel(position->x + w, position->y + h, color);
+		}
+	}
+}
+
+// Same as draw_bitmap_rect, but handles bitmaps stored in PROGMEM
+void draw_bitmap_rect_P(uint16_t *bitmap, Vector2 position, Vector2 *size, Vector2 *rect_pos, Vector2 *rect_size)
+{
+	for (uint16_t h = 0; h < rect_size->y; h++)
+	{
+		for (uint16_t w = 0; w < rect_size->x; w++)
+		{
+			Vector2 img_pos = {rect_pos->x + w, rect_pos->y + h};
+			uint16_t color = pgm_read_dword(VECTOR2_TO_PTR(bitmap, img_pos, size));
+			tft.drawPixel(position.x + w, position.y + h, color);
+		}
+	}
+}
+
+void draw_image(DrawableImage *img)
+{
+	Vector2 positions[] = {
+		{10, 10},
+		{40, 10},
+		{70, 10},
+		{110, 10},
+	};
+	Vector2 zero_pos = {0, 0};
+	Vector2 off_5_pos = {5, 5};
+	Vector2 full_img = {20, 20};
+	Vector2 half_w_img = {10, 20};
+	Vector2 half_h_img = {20, 10};
+	Vector2 half_wh_img = {10, 10};
+
+	draw_bitmap_P((uint16_t *)frogge, positions, &full_img);
+	draw_bitmap_rect_P((uint16_t *)frogge, positions[1], &full_img, &zero_pos, &half_w_img);
+	draw_bitmap_rect_P((uint16_t *)frogge, positions[2], &full_img, &zero_pos, &half_h_img);
+	draw_bitmap_rect_P((uint16_t *)frogge, positions[3], &full_img, &off_5_pos, &half_wh_img);
+}
+
+void undraw_image(DrawableImage *img)
+{
+	draw_bitmap(img->img, &img->position, &img->size);
+}
+
+DrawableImage background{
+	Vector2{0, 0},
+	Vector2{10, 5},
+	&draw_image,
+	nullptr
+	// (uint16_t[50]) {
+	// 	RGB_TO_COLOR(12, 0, 11), RGB_TO_COLOR(12, 0, 12), RGB_TO_COLOR(12, 0, 13), RGB_TO_COLOR(12, 0, 14), RGB_TO_COLOR(12, 0, 15), RGB_TO_COLOR(12, 63, 16), RGB_TO_COLOR(12, 63, 17), RGB_TO_COLOR(12, 63, 18), RGB_TO_COLOR(12, 63, 19), RGB_TO_COLOR(12, 63, 20),
+	// 	RGB_TO_COLOR(14, 0, 11), RGB_TO_COLOR(14, 0, 12), RGB_TO_COLOR(14, 0, 13), RGB_TO_COLOR(14, 0, 14), RGB_TO_COLOR(14, 0, 15), RGB_TO_COLOR(14, 63, 16), RGB_TO_COLOR(14, 63, 17), RGB_TO_COLOR(14, 63, 18), RGB_TO_COLOR(14, 63, 19), RGB_TO_COLOR(14, 63, 20),
+	// 	RGB_TO_COLOR(16, 0, 11), RGB_TO_COLOR(16, 0, 12), RGB_TO_COLOR(16, 0, 13), RGB_TO_COLOR(16, 0, 14), RGB_TO_COLOR(16, 0, 15), RGB_TO_COLOR(16, 63, 16), RGB_TO_COLOR(16, 63, 17), RGB_TO_COLOR(16, 63, 18), RGB_TO_COLOR(16, 63, 19), RGB_TO_COLOR(16, 63, 20),
+	// 	RGB_TO_COLOR(18, 0, 11), RGB_TO_COLOR(18, 0, 12), RGB_TO_COLOR(18, 0, 13), RGB_TO_COLOR(18, 0, 14), RGB_TO_COLOR(18, 0, 15), RGB_TO_COLOR(18, 63, 16), RGB_TO_COLOR(18, 63, 17), RGB_TO_COLOR(18, 63, 18), RGB_TO_COLOR(18, 63, 19), RGB_TO_COLOR(18, 63, 20),
+	// 	RGB_TO_COLOR(20, 0, 11), RGB_TO_COLOR(20, 0, 12), RGB_TO_COLOR(20, 0, 13), RGB_TO_COLOR(20, 0, 14), RGB_TO_COLOR(20, 0, 15), RGB_TO_COLOR(20, 63, 16), RGB_TO_COLOR(20, 63, 17), RGB_TO_COLOR(20, 63, 18), RGB_TO_COLOR(20, 63, 19), RGB_TO_COLOR(20, 63, 20),
+	// }
+};
+
+void init_gfx()
+{
+	tft.begin(); // Initialize the tft
 }
--- lib/gfx.h
+++ lib/gfx.h
@@ -6,10 +6,58 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
+// Images
+#include <avr/pgmspace.h>
+#include "images.h"
+
+// ILI screen resolution
+// 240x320
+
+// A macro converting any rgb pair into a valid color code
+// One must provid one of the following valid ranges, the resulting color is undefined if these ranges are not followed
+// @param r Red color, valid range: [0..31]
+// @param g Green color, valid range: [0..63]
+// @param b Blue color, valid range: [0..31]
+#define RGB_TO_COLOR(r, g, b) ((uint16_t)(r * 2048 + g * 32 + b))
+
+// Translate a pos with a size to a 1D array pointer
+// @param base The pointer to the start of the array
+// @param pos The position in the array (expressed in x/y)
+// @param size The size of the array (expressed in width/height). THIS ASSUMES A POINTER.
+#define VECTOR2_TO_PTR(base, pos, size) (base + (pos.x + pos.y * (size)->x))
+
+// Same as VECTOR2_TO_PTR but it returns the color associated with the pointer.
+// @see VECTOR2_TO_PTR
+#define VECTOR2_TO_COLOR(base, pos, size) ((uint16_t)*VECTOR2_TO_PTR(base, pos, size))
+
+// A simple vector2 struct
+typedef struct Vector2 {
+	uint16_t x;
+	uint16_t y;
+} Vector2;
+
+// A base struct for all drawable things
+#define DRAWABLE_BASE(inherit) \
+	Vector2 position; \
+	Vector2 size; \
+	void (*draw)(inherit *); \
+
+// Image struct
+typedef struct DrawableImage {
+	DRAWABLE_BASE(DrawableImage)
+	uint16_t* img;
+} DrawableImage;
+
+typedef struct DrawableImageRect {
+	DRAWABLE_BASE(DrawableImageRect)
+	uint16_t* img;
+	Vector2 rect_position;
+	Vector2 rect_size;
+} DrawableImageRect;
+
 // The screen, it's not adviced to modify the screen directly, but rather use the functions defined here
 extern Adafruit_ILI9341 tft;
 
-// Start the gfx library
 void init_gfx();
 
 #endif
