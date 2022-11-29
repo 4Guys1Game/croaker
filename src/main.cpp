dae47dd8783d43d921b98615d10b2daf6a1b74de
jaschutte <jaschutte49@gmail.com>
2022-11-29 15:46:16 +0100

    prep demo 1

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@
 #define __AVR_ATmega328P__
 #endif
 
- #include <avr/io.h>
- #include <avr/eeprom.h>
- #include <avr/interrupt.h>
- #include <HardwareSerial.h>
- #include <nunchuck_value.h>
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <HardwareSerial.h>
+#include <nunchuck_value.h>
+#include <avr/delay.h>
+
+#include "global_time.h"
+#include "gfx.h"
+#include "ir.h"
 
 // Baudrate for Serial communication
 #define BAUDRATE 9600
@@ -19,26 +24,73 @@
 #define NUNCHUK_ADDRESS 0x52
 
 // Functions for reading and writing values to the EEPROM.
-#define load_value(address) 	   eeprom_read_byte((uint8_t *)address)
+#define load_value(address) eeprom_read_byte((uint8_t *)address)
 #define save_value(address, value) eeprom_write_byte((uint8_t *)address, value)
 
 // An enum for the different addresses to read or write to/from on the EEPROM
-enum eeprom_location { HIGH_SCORE = EEAR0, OPPONENT_HIGH_SCORE = EEAR0 };
+enum eeprom_location
+{
+	HIGH_SCORE = EEAR0,
+	OPPONENT_HIGH_SCORE = EEAR0
+};
 
 int main(void)
 {
-    // Enable interrupts
-    sei();
+	// Init
+	setup_global_timer();
+	init_gfx();
+	init_ir(FREQ_VAL_38KHZ);
 
-    // Begin Serial communication
-    Serial.begin(BAUDRATE);
+	sei();
 
-    // Initialize the connection with the nunchuk and stop if it is not found
-    if (!init_nunchuk(NUNCHUK_ADDRESS)) return 1;
-    
-    // Repeat forever
-    while (1) {}
+	// Begin Serial communication
+	// Serial.begin(BAUDRATE);
 
-    // This is never reached.
-    return 0;
+
+	// Initialize the connection with the nunchuk and stop if it is not found
+	// if (!init_nunchuk(NUNCHUK_ADDRESS)) return 1;
+
+	// Draw the background
+	draw_tilemap(background);
+	uint32_t next_message = 0;
+	uint32_t next_move = 0;
+
+	Vector2 start_pos = {4 * 20, 15 * 20};
+	Vector2 end_pos = {6 * 20, 10 * 20};
+	NEW_IMAGE(main_character, start_pos.x, start_pos.y, image_frogge);
+
+	uint8_t is_at_end = false;
+
+	// Main game loop
+	while (1)
+	{
+
+		if (global_time >= next_move)
+		{
+			if (is_at_end)
+			{
+				is_at_end = false;
+				move_image(&main_character, &start_pos);
+			}
+			else
+			{
+				is_at_end = true;
+				move_image(&main_character, &end_pos);
+			}
+			next_move = global_time + 250;
+		}
+
+		// Constantly send IR messages
+		if (global_time >= next_message)
+		{
+			next_message = global_time + 500;
+			ir_send_message(2);
+		}
+
+		// Update the IR
+		ir_heartbeat();
+	}
+
+	// This is never reached.
+	return 0;
 }
