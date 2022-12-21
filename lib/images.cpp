ab9c8d7acdc15701919f05454e486fa61ac131c9
ItsGers8 <gerson@makweb.nl>
2022-12-21 11:04:16 +0100

    reworked all the sprites

--- lib/images.cpp
+++ lib/images.cpp
@@ -4,55 +4,13 @@
 // AUTO GENERATED IMAGE, PUT ME IN images.cpp
 RawImage image_frogge {
 	GET_IMAGE(frogge),
-	126,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_grass {
-	GET_IMAGE(grass),
-	88,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_grass_lake {
-	GET_IMAGE(grass_lake),
-	58,
+	124,
 	Vector2 { 20, 20 }
 };
 // AUTO GENERATED IMAGE, PUT ME IN images.cpp
 RawImage image_lake {
 	GET_IMAGE(lake),
 	43,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_lake_sand {
-	GET_IMAGE(lake_sand),
-	54,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_road_grass {
-	GET_IMAGE(road_grass),
-	59,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_road_middle {
-	GET_IMAGE(road_middle),
-	45,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_sand_grass {
-	GET_IMAGE(sand_grass),
-	74,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_sidewalk_road {
-	GET_IMAGE(sidewalk_road),
-	70,
 	Vector2 { 20, 20 }
 };
 // AUTO GENERATED IMAGE, PUT ME IN images.cpp
@@ -74,12 +32,6 @@ RawImage image_log_back {
 	Vector2 { 20, 20 }
 };
 // AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_grass_road {
-	GET_IMAGE(grass_road),
-	80,
-	Vector2 { 20, 20 }
-};
-// AUTO GENERATED IMAGE, PUT ME IN images.cpp
 RawImage image_log_middle {
 	GET_IMAGE(log_middle),
 	122,
@@ -96,4 +48,40 @@ RawImage image_croaker_logo {
 	GET_IMAGE(croaker_logo),
 	1048,
 	Vector2 { 171, 44 }
+};
+// AUTO GENERATED IMAGE, PUT ME IN images.cpp
+RawImage image_road {
+	GET_IMAGE(road),
+	48,
+	Vector2 { 20, 20 }
+};
+// AUTO GENERATED IMAGE, PUT ME IN images.cpp
+RawImage image_grass_road {
+	GET_IMAGE(grass_road),
+	66,
+	Vector2 { 20, 20 }
+};
+// AUTO GENERATED IMAGE, PUT ME IN images.cpp
+RawImage image_road_sand_water {
+	GET_IMAGE(road_sand_water),
+	78,
+	Vector2 { 20, 20 }
+};
+// AUTO GENERATED IMAGE, PUT ME IN images.cpp
+RawImage image_water_sand_grass {
+	GET_IMAGE(water_sand_grass),
+	60,
+	Vector2 { 20, 20 }
+};
+// AUTO GENERATED IMAGE, PUT ME IN images.cpp
+RawImage image_grass_flower {
+	GET_IMAGE(grass_flower),
+	64,
+	Vector2 { 20, 20 }
+};
+// AUTO GENERATED IMAGE, PUT ME IN images.cpp
+RawImage image_grass_bland {
+	GET_IMAGE(grass_bland),
+	43,
+	Vector2 { 20, 20 }
 };
