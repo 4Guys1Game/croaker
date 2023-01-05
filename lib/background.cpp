43540f9ffe892383c40c657fb08a016a0785eb8a
jaschutte <jaschutte49@gmail.com>
2023-01-05 15:34:28 +0100

    level 2

--- lib/background.cpp
+++ lib/background.cpp
@@ -15,9 +15,7 @@
 #define __BG_GRASS_1(a, b) __BG_TILE(a, a), __BG_TILE(a, a), __BG_TILE(b, a), __BG_TILE(a, a), __BG_TILE(a, a), __BG_TILE(a, b),
 #define __BG_GRASS_2(a, b)  __BG_TILE(b, a), __BG_TILE(a, a), __BG_TILE(a, a), __BG_TILE(b, b), __BG_TILE(a, a), __BG_TILE(a, a),
 
-uint8_t current_level = 1;
-
-Level;
+uint8_t current_level = 2;
 
 Level levels[] = {
     {
@@ -108,6 +106,89 @@ Level levels[] = {
 				__BG_ROW(5)
 				__BG_ROW(6)
                 __BG_GRASS_2(1, 7)
+            },
+            {
+                &image_grass_bland,
+                &image_water_sand_grass,
+                &image_lake,
+                &image_road_sand_water,
+                &image_road,
+                &image_grass_road,
+                &image_grass_flower
+            }
+        },
+        {
+            {
+                __BG_ROW(0) __BG_ROW(0) __BG_ROW(0) __BG_ROW(0)
+                __BG_ROW(0) __BG_ROW(0) __BG_ROW(0) __BG_ROW(0)
+                __BG_ROW(0) __BG_ROW(0) __BG_ROW(0) __BG_ROW(0)
+            },
+            {
+                &image_car_front, &image_car_back, &image_log_front,
+                &image_log_middle, &image_log_back
+            }
+        },
+        {
+            3,
+            19,
+            {
+                { 0, 0 },
+                { 1, 8 },
+                
+				{ 0, 1 },
+                { 1, 5 },
+
+                { 0, 2 },
+                { 0, 5 },
+                { 1, 8 },
+
+                { 1, 1 },
+                
+				{ 0, 6 },
+				{ 1, 9 },
+
+				{ 1, 3 },
+
+				{ 1, 6 },
+
+				{ 1, 1 },
+
+				{ 0, 8 },
+				{ 0, 1 },
+				{ 1, 5 },
+
+				{ 0, 2 },
+				{ 1, 8 },
+
+				{ 1, 12 },
+            }
+        },
+        {
+            0,
+            0,
+			{}
+        }
+    },
+
+	{
+        {
+            {
+                __BG_ROW(0)
+				__BG_ROW(0)
+				__BG_ROW(4)
+				__BG_ROW(5)
+                __BG_ROW(5)
+				__BG_ROW(5)
+				__BG_ROW(6)
+				__BG_ROW(2)
+                __BG_ROW(3)
+				__BG_ROW(3)
+				__BG_ROW(3)
+				__BG_ROW(4)
+                __BG_ROW(5)
+				__BG_ROW(5)
+				__BG_ROW(5)
+                __BG_ROW(6)
             },
             {
                 &image_grass_bland,
