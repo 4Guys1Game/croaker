beb16474fbfa4663c0a14bbcac26d1606bc58b39
jaschutte <jaschutte49@gmail.com>
2023-01-06 12:13:07 +0100

    removed unneccessary definitions

--- lib/spi_basics.h
+++ lib/spi_basics.h
@@ -4,12 +4,6 @@
 
 #include <avr/io.h>
 
-void output_dc_cs();
-void input_dc_cs();
-void set_dc_high();
-void set_dc_low();
-void set_cs_high();
-void set_cs_low();
 void spi_write(uint8_t data);
 void spi_write16(uint16_t data);
 
